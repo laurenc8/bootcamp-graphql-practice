508b953e00a001c1803b6b5097033397a570f24c
laurenc8 <laurenc8822@gmail.com>
2021-01-15 02:33:19 -0500

    typedefs

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -3,5 +3,56 @@ const { gql } = require('apollo-server-express')
 module.exports = gql`
   type Query {
     welcome: String!
+    authorById(id: ID!): Author!
+    bestsellers: [Book!]
+    bigPublishers: [Publisher!]
+  }
+  type Mutation {
+    addAuthor(input: addAuthorInput): Author!
+    deletePublisher(input: deletePublisherInput): Publisher!
+  }
+  input addAuthorInput {
+    firstName: String!
+    lastName: String!
+    age: Int!
+    email: String!
+    numBooksPublished: String!
+    address: Address!
+  }
+  input deletePublisherInput {
+    id: ID!
+    company: String!
+    phoneNumber: String!
+    numBooksPublished: Int!
+    address: Address!
+  }
+  type Author {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    age: Int!
+    email: String!
+    numBooksPublished: String!
+    address: Address!
+  }
+  type Address {
+    id: ID!
+  }
+  type Book {
+    id: ID!
+    title: String!
+    language: String!
+    numPages: Int!
+    datePublished: Int!
+    bestseller: Boolean!
+    author: Author!
+    publisher: Publisher!
+  }
+  type Publisher {
+    id: ID!
+    company: String!
+    phoneNumber: String!
+    numBooksPublished: Int!
+    address: Address!
   }
 `
