34bc26d46dad62556351fe72c886969ef25d77ae
laurenc8 <laurenc8822@gmail.com>
2021-01-17 23:50:14 -0500

    typedefs

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -2,53 +2,63 @@ const { gql } = require('apollo-server-express')
 
 module.exports = gql`
   type Query {
-    welcome: String!
-    authorById(id: ID!): Author!
+    # welcome: String!
+    author(id: ID!): Author!
     bestsellers: [Book!]
     bigPublishers: [Publisher!]
+    searchAuthors(input: String!): [Author!]!
   }
   type Mutation {
     addAuthor(input: addAuthorInput): Author!
     deletePublisher(input: deletePublisherInput): Publisher!
   }
-  input addAuthorInput {
-    firstName: String!
-    lastName: String!
-    age: Int!
-    email: String!
-    numBooksPublished: String!
-    address: Address!
+  type Address {
+    id: ID!
+    street: String!
+    city: String!
+    state: String!
+    zip: String!
   }
-  input deletePublisherInput {
+  type Publisher {
     id: ID!
     company: String!
     phoneNumber: String!
-    numBooksPublished: Int!
-    address: Address!
+    numBooksPublished: Int
+    address: Address
+    books: [Book!]!
+    createdAt: String!
   }
   type Author {
     id: ID!
     firstName: String!
     lastName: String!
-    age: Int!
-    email: String!
-    numBooksPublished: String!
-    address: Address!
-  }
-  type Address {
-    id: ID!
+    age: Int
+    email: String
+    numBooksPublished: String
+    address: Address
+    books: [Book!]!
+    createdAt: String!
   }
   type Book {
     id: ID!
     title: String!
     language: String!
-    numPages: Int!
-    datePublished: Int!
-    bestseller: Boolean!
+    numPages: Int
+    datePublished: Int
+    bestseller: Boolean
     author: Author!
     publisher: Publisher!
+    createdAt: String!
   }
-  type Publisher {
+  input addAuthorInput {
+    firstName: String!
+    lastName: String!
+    age: Int
+    email: String
+    numBooksPublished: Int
+    address: Address!
+  }
+  input deletePublisherInput {
     id: ID!
     company: String!
     phoneNumber: String!
